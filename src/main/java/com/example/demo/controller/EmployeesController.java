e7e52099de4e64b76071ab691c20ab8818dc6c3d
Hemant <hk6820@gmail.com>
2021-09-01 11:08:45 +0530

    delete mapping

--- src/main/java/com/example/demo/controller/EmployeesController.java
+++ src/main/java/com/example/demo/controller/EmployeesController.java
@@ -5,9 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,6 +38,12 @@ public class EmployeesController {
 	@GetMapping("/employees/{id}")
 	public ResponseEntity<Employee> readEmployee (@PathVariable Long id) {
 		return new ResponseEntity<Employee>(nRepo.findById(id).get(), HttpStatus.OK);
+	}
+
+@DeleteMapping("/employees/{id}")
+	public ResponseEntity<HttpStatus> deleteEmployee (@PathVariable Long id) {
+		nRepo.deleteById(id);
+		return new ResponseEntity<HttpStatus>(HttpStatus.NO_CONTENT);
 	}
 }
 
